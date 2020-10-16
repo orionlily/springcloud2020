bd5d6639641a4d254a28edcf8734561f1a55aea6
li.lc <li.lc@starcart.cn>
2020-10-16 19:35:59 +0800

    nacos

---
+++ cloud-alibaba-consumer-order83/src/main/java/com/orion/alibaba/consumer/OrderMain83.java
@@ -0,0 +1,16 @@
+package com.orion.alibaba.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author li.lc
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain83.class,args);
+    }
+}
