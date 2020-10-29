3e46f101485cf99727bab80876a3884d7a1332cf
orionlily <gdoullc@163.com>
2020-10-30 01:23:21 +0800

    seata order

---
+++ cloud-alibaba-seata-order2001/src/main/java/com/orion/alibaba/seata/OrderMain2001.java
@@ -0,0 +1,19 @@
+package com.orion.alibaba.seata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Administrator
+ * @date 2020/10/30
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class OrderMain2001 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain2001.class,args);
+    }
+}
